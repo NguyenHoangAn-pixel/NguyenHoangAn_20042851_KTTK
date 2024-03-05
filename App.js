9dc8ca0edc38a91247c4a5cb90ca7d0f84d1d317
H82M19\Student <Student>
2024-03-05 08:29:46 +0700

    Cau1

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeSrceen from './View/Cau1';
+import DetailHome from './components/DetailScreen';
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <Screen01 />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home' screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name='Cau1' component={Cau1} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ View/Cau1.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, Animated } from 'react-native';
+
+const App = () => {
+  const [opacityValue] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    animateText();
+  }, []);
+
+  const animateText = () => {
+    Animated.sequence([
+      Animated.timing(opacityValue, {
+        toValue: 0.5,
+        duration: 1000,
+        useNativeDriver: false,
+      }),
+      Animated.timing(opacityValue, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: false,
+      }),
+    ]).start();
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Animated.Text
+        style={{
+          fontSize: 20,
+          opacity: opacityValue,
+        }}
+      >
+        Hello 1
+      </Animated.Text>
+      <View style={{ height: 20 }} />
+      <Animated.Text
+        style={{
+          fontSize: 20,
+          opacity: opacityValue,
+        }}
+      >
+       Hello 2
+      </Animated.Text>
+    </View>
+  );
+};
+
+export default App;
